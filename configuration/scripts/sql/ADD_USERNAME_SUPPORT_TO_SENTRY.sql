747c2eaabdeb6a2aaa16269a316e383857f529c8
Benji Schwartz-Gilbert <benji.schwartzgilbert@gmail.com>
2014-03-21 21:57:15 -0700

    Updates the username column add script to insert it after the id column

--- configuration/scripts/sql/ADD_USERNAME_SUPPORT_TO_SENTRY.sql
+++ configuration/scripts/sql/ADD_USERNAME_SUPPORT_TO_SENTRY.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `users` ADD `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE `users` ADD `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL AFTER `id`;
 ALTER TABLE `users` ADD CONSTRAINT UNIQUE KEY `users_username_unique` (`username`);
